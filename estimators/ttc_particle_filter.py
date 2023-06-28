9aee5f38499ad62aa53996829564e45cbaa3ae3e
James Adams <bmcc1745@gmail.com>
2023-06-28 10:37:17 -0600

    Fixed merge error

--- estimators/ttc_particle_filter.py
+++ estimators/ttc_particle_filter.py
@@ -13,13 +13,6 @@ class TTCParticleFilter:
         self.num_particles = 1000
         self.bearing_std = 0.01
         self.yaw_std = 0.01
-<<<<<<< HEAD
-        self.tau_resample_std = 0.5
-        self.vi_resample_std = 0.5
-        self.yaw_resample_std = 0.1
-        self.Rinv = np.diag([1/self.bearing_std**2, 1/self.yaw_std**2])
-        self.Qinv = np.diag([1/self.bearing_std**2, 1/self.tau_resample_std**2, 1/self.vi_resample_std**2, 1/self.yaw_resample_std**2, 1/self.tau_resample_std**2])
-=======
         self.Rinv = np.diag([1/self.bearing_std**2, 1/self.yaw_std**2])
         self.tau_pr_noise = 0.1
         self.vi_pr_noise = 5.
@@ -34,7 +27,6 @@ class TTCParticleFilter:
                            [1.,0.],
                            [0.,1.],
                            [0.,0.]]) * np.sqrt(self.ts)
->>>>>>> d6923400762a057f1575cd1dc3d2b70b0a63806b
         tau_min = 5
         tau_max = 100
         vi_max = 50
@@ -42,22 +34,12 @@ class TTCParticleFilter:
         self.xhats = np.zeros((5, self.num_particles))
         self.weights = np.zeros((self.num_particles))
         for i in range(self.num_particles):
-<<<<<<< HEAD
-            particle = Particle(np.random.normal(initial_bearing, self.bearing_std),#initial_bearing,#
-                                (tau_max - tau_min)*np.random.rand()+tau_min,
-                                (vi_max-vi_min)*np.random.rand()+vi_min,
-                                2*np.pi*np.random.rand(),
-                                np.random.normal(initial_yaw, self.yaw_std), #initial_yaw,#
-                                ts)
-            self.particles.append(particle)
-=======
             self.xhats[:,i] = np.array([initial_bearing,#np.random.normal(initial_bearing, self.bearing_std),
                                 (tau_max - tau_min)*np.random.rand()+tau_min,
                                 (vi_max-vi_min)*np.random.rand()+vi_min,
                                 2*np.pi*np.random.rand(),
                                 initial_yaw,#np.random.normal(initial_yaw, self.yaw_std)
                                 ])
->>>>>>> d6923400762a057f1575cd1dc3d2b70b0a63806b
 
     def update(self, measurement:BearingMsg, state:TwoDYawState, input:float):
         self.propagate_model(state, input)
@@ -80,45 +62,6 @@ class TTCParticleFilter:
     def measurement_update(self, measurement:BearingMsg):
         for i in range(self.num_particles):
             y = np.array([[measurement.bearing, measurement.yaw]]).T
-<<<<<<< HEAD
-            h = np.array([[particle.xhat.item(0), particle.xhat.item(4)]]).T
-            particle.weight *= np.exp(-1/2. * (y-h).T @ self.Rinv @ (y-h)).item(0)
-
-
-    def resample(self, measurement:BearingMsg):
-        sum_of_weights = 0
-        weights = []
-        for particle in self.particles:
-            sum_of_weights += particle.weight
-            weights.append(particle.weight)
-        
-        weights = np.array(weights)
-        norm_weights = weights/sum_of_weights
-        norm_weights[-1] = 1.
-        for i in range(1,self.num_particles):
-            norm_weights[i] += norm_weights[i-1]
-        rand = np.linspace(0, 1, self.num_particles)#np.random.rand(self.num_particles)
-        # rand = np.sort(rand)
-
-        old_particles = deepcopy(self.particles)
-        self.particles.clear()
-
-        i = 0 # random number
-        j = 0 # particle to be sampled
-        while i < self.num_particles:
-            while rand[i] > norm_weights[j]:
-                j += 1
-            old_particle = old_particles[j]
-            particle = Particle(np.random.normal(old_particle.xhat.item(0), self.bearing_std),#measurement.bearing,#
-                                np.random.normal(old_particle.xhat.item(1), self.tau_resample_std),
-                                np.random.normal(old_particle.xhat.item(2), self.vi_resample_std),
-                                np.random.normal(old_particle.xhat.item(3), self.yaw_resample_std),
-                                np.random.normal(old_particle.xhat.item(4), self.yaw_std),#measurement.yaw,#
-                                self.ts)
-            # particle.weight = np.exp(-1/2*(particle.xhat - old_particle.xhat).T @ self.Qinv @ (particle.xhat - old_particle.xhat)).item(0)
-            self.particles.append(particle)
-            i += 1
-=======
             h = np.array([[self.xhats[:,i].item(0), self.xhats[:,i].item(4)]]).T
             self.weights[i] = np.exp(-1/2. * (y-h).T @ self.Rinv @ (y-h)).item(0)
         # normalize the weights
@@ -157,7 +100,6 @@ class TTCParticleFilter:
         # calculate xdot
         xdot = np.array([[sin(eta)/tau-vi*sin(eta+psi-psii)/(vo*tau)-psid,-cos(eta)+vi/vo*cos(eta+psi-psii), 0., 0., psid]]).T
         return xdot
->>>>>>> d6923400762a057f1575cd1dc3d2b70b0a63806b
 
     def get_particle_states(self, uav_state:TwoDYawState):
         states = []
